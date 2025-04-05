fff02f0d523e3fbfc22ceefd9cc2bea9a70f64a6
Shashank Singh <shashankrock24@gmail.com>
2025-04-05 17:32:55 +0530

    Create container.go

---
+++ pkg/container/container.go
@@ -0,0 +1,45 @@
+package container
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+type Container struct {
+	ID      string
+	Name    string
+	Image   string
+	Command string
+	Status  string
+}
+
+func (c *Container) PrepareBundle() error {
+	bundleDir := filepath.Join("/var/lib/containerhub", c.ID)
+	if err := os.MkdirAll(bundleDir, 0755); err != nil {
+		return err
+	}
+
+	config := map[string]interface{}{
+		"ociVersion": "1.0.2",
+		"process": map[string]interface{}{
+			"terminal": false,
+			"user":     map[string]int{"uid": 0, "gid": 0},
+			"args":     []string{"sh", "-c", c.Command},
+			"env":      []string{"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"},
+		},
+		"root": map[string]interface{}{
+			"path": c.Image,
+		},
+		"hostname": c.Name,
+		"mounts":   []interface{}{},
+	}
+
+	configFile, err := os.Create(filepath.Join(bundleDir, "config.json"))
+	if err != nil {
+		return err
+	}
+	defer configFile.Close()
+
+	return json.NewEncoder(configFile).Encode(config)
+}
